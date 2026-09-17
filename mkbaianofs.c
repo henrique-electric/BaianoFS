0fb078f57040d35ae1b2939fd851316a841c18cd
Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-09-16 21:33:41 -0300

    Add enums

--- mkbaianofs.c
+++ mkbaianofs.c
@@ -9,6 +9,11 @@
 #define BAIANOFS_MAGIC 0x004F4E41494142
 #define MAX_INODE_COUNT 200
 
+enum inode_type {
+	INODE_DIR,
+	INODE_FILE,
+	INODE_NOT_INIT
+};
 
 struct superblock {
 	int64_t magic;
@@ -20,6 +25,8 @@ struct superblock {
 
 struct inode_struct {
 	uint32_t size;
+	uint32_t data_block;
+	uint8_t  inode_type;
 	char name[255];
 } __attribute__((packed));
 
@@ -53,9 +60,12 @@ int main(int argc, char *argv[])
 		printf("Error allocating buffer to write inode data\n");
 		exit(EXIT_FAILURE);
 	}
+	
 
+	// Allocate 200 inodes, not that much but i think it's good for a test filesystem
 	for (int i = 0; i < MAX_INODE_COUNT; i ++) {
 		__inode_struct->size = 0;
+		__inode_struct->inode_type = INODE_NOT_INIT,
 		memset(__inode_struct->name, 0x0, 255);
 		lseek(device, BAIANOFS_BLSIZE, SEEK_CUR);
 		write(device, (void*) __inode_struct, sizeof(struct inode_struct));
